88155c9f2f05b72f2fc0819d1344e46dcea3806d
Leonard <leonardwkee@gmail.com>
2021-11-11 15:54:45 -0800

    update title of app

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -1,5 +1,5 @@
-import React from 'react';
+import React from 'react'
 
 export default function Icon() {
-  return (<img src='./images/icon-search.svg' alt="search" />);
+  return (<img src='./images/icon-search.svg' alt="search" />)
 }
